f20a24fa4673baa8540e765ed97da7e6845120a4
이원준 <zzzdnjswns@naver.com>
2021-09-17 00:38:36 +0900

    난수입력

---
+++ Doit/sum1ton.py
@@ -0,0 +1,31 @@
+# print('1부터 n까지 정수의 합을 구합니다.')
+
+# n = int(input('n값을 입력하세여.:'))
+
+# sum = 0
+# i = 1
+
+# while i <= n:
+#     sum += i
+#     i += 1
+
+# print(f'i값은 {i}입니다.')
+# print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+
+# print('1부터 n까지 정수의 합을 구합니다.')
+
+# n = int(input('n값을 입력하세요.:'))
+
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += i
+
+# print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+
+print('1부터 n까지 정수으 합을 구합니다.')
+
+n = int(input('n값을 입력하세요.'))
+
+sum = n * (n + 1) // 2
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
