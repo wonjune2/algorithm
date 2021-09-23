d3b24c3566e1496eda89d801b25fc93f27aafae4
이원준 <zzzdnjswns@naver.com>
2021-09-24 00:25:37 +0900

    Doit 함수 사이 인수 주고받기

--- Doit/2_7card_conv.py
+++ Doit/2_7card_conv.py
@@ -5,8 +5,14 @@ def card_conv(x: int, r: int) -> str:
 
     d = ''
     dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+    n = len(str(x))
+    print(f'{r:2} | {x:{n}d}')
     while x > 0:
+        print('    +' + (n + 2) * '-')
+        if x // r:
+            print(f'{r:2} | {x // r:{n}d} ... {x % r}')
+        else :
+            print(f'   | {x // r:{n}d} ... {x % r}')
         d += dchar[x % r]
         x //= r
         
