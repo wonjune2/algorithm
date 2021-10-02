f41803ac7dfb85447196ced78c0d601b3e1155fa
이원준 <zzzdnjswns@naver.com>
2021-10-02 23:34:06 +0900

    Doit 해시 체인법

--- Doit/해시법/3_5Achained_hash.py
+++ Doit/해시법/3_5Achained_hash.py
@@ -49,6 +49,33 @@ class ChainedHash:
                 return False    # 추가 실패
             p = p.next          # 뒤쪽 노드 주목
         
-        temp = None(key, value, self.table[hash])
+        temp = Node(key, value, self.table[hash])
         self.table[hash] = temp # 노드를 추가
         return True             # 추가 성공
+
+    def remove(self, key:Any) -> bool:
+        """키가 key인 원소를 삭제"""
+        hash = self.hash_value(key) # 삭제할 key의 해시값
+        p = self.table[hash]        # 노드를 주목
+        pp = None                   # 바로 앞의 노드를 주목
+
+        while p is not None:
+            if p.key == key:
+                if pp is None:
+                    self.table[hash] = p.next
+                else:
+                    pp.next = p.next
+                return True
+            pp = p
+            p = p.next  # 뒤쪽 노드를 주목
+        return False    # 삭제 실패(key가 존재하지 않음)
+
+    def dump(self) -> None:
+        """해시 테이블을 덤프"""
+        for i in range(self.capacity):
+            p = self.table[i]
+            print(i, end='')
+            while p is not None:
+                print(f'  ->{p.key} ({p.value})', end='')
+                p = p.next
+            print()
