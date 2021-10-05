f2a99ac5b1b9e4299412393d7fcd68201cb6fafa
이원준 <zzzdnjswns@naver.com>
2021-10-05 22:55:45 +0900

    프로그래머스 로또 번호

--- backjoon/재귀/10870.py
+++ backjoon/재귀/10870.py
@@ -9,3 +9,13 @@ def fib(n):
 n = int(input())
 
 print(fib(n))
+
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1 or n == 2:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+
+# print(fib(3))
