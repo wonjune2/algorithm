f302008ef178c4869eada9b6c9f41ef45319a483
이원준 <zzzdnjswns@naver.com>
2021-10-07 01:06:23 +0900

    Doit 스택

--- Doit/스택큐/fixed_stack.py
+++ Doit/스택큐/fixed_stack.py
@@ -30,3 +30,20 @@ class FixedStack:
     def is_full(self) -> bool:
         """스택이 가득 차 있는지 판단"""
         return self.ptr >= self.capacity
+    
+    def push(self, value: Any) -> None:
+        """스택에 value를 푸시(데이터를 넣음)"""
+        if self.is_full():          # 스택이 가득 차 있는 경우
+            raise FixedStack.Full   # 예외 처리 발생
+        self.stk[self.ptr] = value
+        self.ptr += 1
+    
+    def pop(self) -> Any:
+        """스택에서 데이터를 팝(꼭대기 데이터를 꺼냄)"""
+        if self.is_empty():         # 스택이 비어 있는 경우
+            raise FixedStack.Empty  # 예외 처리 발생
+        return self.stk[self.ptr - 1]
+
+    def clear(self) -> None:
+        """스택을 비움(모든 데이터를 삭제)"""
+        self.ptr = 0
