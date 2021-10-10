545cacf147f1f9cae755d8c082bc7daa7cd7a728
이원준 <zzzdnjswns@naver.com>
2021-10-10 22:27:25 +0900

    없는 숫자 더하ㅏ

---
+++ programmers/없는 숫자 더하기.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = 0
+    add = []
+    for i in range(10):
+        if i in numbers:
+            pass
+        else:
+            answer += i
+    print(answer)
+    return answer
+
+num = [1,2,3,4,6,7,8,0]
+solution(num)
--- programmers/크레인 인형뽑기 게임.py
+++ programmers/크레인 인형뽑기 게임.py
@@ -24,5 +24,4 @@ def solution(board, moves):
 test = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 mov = [1, 5, 3, 5, 1, 2, 1, 4]
 # mov = [1]
-
 print(solution(test, mov))
