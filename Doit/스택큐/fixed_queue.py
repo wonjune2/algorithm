2a631f77d887284a0b694cacfe33501945f6bbea
이원준 <zzzdnjswns@naver.com>
2021-10-12 00:48:34 +0900

    Doit 디큐

--- Doit/스택큐/fixed_queue.py
+++ Doit/스택큐/fixed_queue.py
@@ -32,7 +32,7 @@ class FixedQueue:
         """큐가 가득 차 있는지 판단"""
         return self.no >= self.capacity
 
-    def  enque(self, x: Any) -> None:
+    def enque(self, x: Any) -> None:
         """데이터 x를 인큐"""
         if self.is_full():
             raise FixedQueue.Full # 큐가 가득 차 있는 경우 예외 처리 발생
@@ -41,3 +41,14 @@ class FixedQueue:
         self.no += 1
         if self.rear == self.capacity:
             self.rear = 0
+
+    def deque(self) -> Any:
+        """데이터 디큐"""
+        if self.is_empty():
+            raise FixedQueue.Empty  # 큐가 비어 있는 경우 예외 처리 발생
+        x = self.que[self.front]
+        self.front += 1
+        self.no -= 1
+        if self.front == self.capacity:
+            self.front = 0
+        return x
