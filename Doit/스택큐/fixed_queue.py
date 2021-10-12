5f3f3e28bc8d9c40a534c6cc76fc0213468d1540
이원준 <zzzdnjswns@naver.com>
2021-10-12 23:36:27 +0900

    Doit 큐 완성본

--- Doit/스택큐/fixed_queue.py
+++ Doit/스택큐/fixed_queue.py
@@ -52,3 +52,43 @@ class FixedQueue:
         if self.front == self.capacity:
             self.front = 0
         return x
+
+    def peek(self) -> Any:
+        """큐에서 데이터를 픽(맨 앞 데이터를 들여다봄)"""
+        if self.is_empty():
+            raise FixedQueue.Empty  # 큐가 비어있음
+        return self.que[self.front]
+
+    def find(self, value: Any) -> Any:
+        """큐에서 value를 찾아 인덱스를 반환(없으면 -1을 반환)"""
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity
+            if self.que[idx] == value:  # 검색 성공
+                return idx
+            return -1                   # 검색 실패
+
+    def count(self, value: Any) -> bool:
+        """큐에 있는 value의 개수를 반환"""
+        c = 0
+        for i in range(self.no):
+            idx = (i + self.front) % self.capacity
+            if self.que[idx] == value:
+                c += 1
+            return c
+
+    def __contains__(self, value: Any) -> bool:
+        """큐에 value가 있는지 판단"""
+        return self.count(value)
+
+    def clear(self) -> None:
+        """큐의 모든 데이터를 비움"""
+        self.no = self.front = self.rear = 0
+
+    def dump(self) -> None:
+        """모든 데이터를 맨 앞부터 맨 끝 순으로 출력"""
+        if self.is_empty():
+            print('큐가 비었습니다.')
+        else:
+            for i in range(self.no):
+                print(self.que[i + self.front % self.capacity], end='')
+            print()
