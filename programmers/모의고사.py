18958521ca63d514c8c839ca8dcea60544563ddf
이원준 <zzzdnjswns@naver.com>
2021-10-15 18:59:39 +0900

    모의고사

---
+++ programmers/모의고사.py
@@ -0,0 +1,28 @@
+def solution(answers):
+    answer = []
+    one = [1,2,3,4,5]
+    two = [2,1,2,3,2,4,2,5]
+    three = [3,3,1,1,2,2,4,4,5,5]
+    res = [0] * 3
+    for i in range(len(answers)):
+        if answers[i] == one[i % len(one)]:
+            res[0] += 1
+        if answers[i] == two[i % len(two)]:
+            res[1] += 1
+        if answers[i] == three[i % len(three)]:
+            res[2] += 1
+    answer = [i+1 for i, v in enumerate(res) if v == max(res)]
+    print(answer)
+    return answer
+
+answers = [1,2,3,4,5]
+solution(answers)
+# one = [1,2,3,4,5]
+# for i in range(len(answers)):
+#     print(one[i % len(one)], end='')
+# print()
+# for i in range(len(answers)):
+#     print(answers[i], end='')
+# a = 12
+# b = 12
+# print(max(a, b))
