de4c8fa982849a8408d709f15fe1f5d04075e685
이원준 <zzzdnjswns@naver.com>
2021-10-22 00:49:04 +0900

    Doit 8개의 퀸 알고리즘 구현

--- Doit/재귀/8queen.py
+++ Doit/재귀/8queen.py
@@ -1,6 +1,9 @@
-# 각 열에 퀸을 1개 배치하는 조합을 재귀적으로 나열하기
+# 8퀸 문제 알고리즘 구현하기
 
-pos = [0] * 8   # 각 열에서 퀸의 위치를 출력
+pos = [0] * 8
+flag_a = [False] * 8
+flag_b = [False] * 15
+flag_c = [False] * 15
 
 def put() -> None:
     """각 열에 배치한 퀸의 위치를 출력"""
@@ -9,12 +12,15 @@ def put() -> None:
     print()
 
 def set(i: int) -> None:
-    """i열에 퀸을 배치"""
+    """i열의 알맞은 위치에 퀸을 배치"""
     for j in range(8):
-        pos[i] = j
-        if i == 7:
-            put()
-        else:
-            set(i + 1)
-
-set(0) # 0열에 퀸을 배치
+        if (not flag_a[j] and not flag_b[i + j]
+            and not flag_c[i - j + 7]):
+            pos[i] = j
+            if i == 7:
+                put()
+            else:
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = True
+                set(i + 1)
+                flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = False
+set(0)
