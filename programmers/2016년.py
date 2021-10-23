c12107f01a9f059b1656088e58b1140f34a4a908
이원준 <zzzdnjswns@naver.com>
2021-10-23 23:04:34 +0900

    2016년

--- programmers/2016년.py
+++ programmers/2016년.py
@@ -1,6 +1,7 @@
 import datetime
-answer = ''
-day = ['MON','TUE','WED','THU','FRI','SAT','SUN']
-n = datetime.datetime(2016,6,7).weekday()
-answer = day[n]
-print(answer)
+def solution(a, b):
+    answer = ''
+    day = ['MON','TUE','WED','THU','FRI','SAT','SUN']
+    n = datetime.datetime(2016,a,b).weekday()
+    answer = day[n]
+    return answer
