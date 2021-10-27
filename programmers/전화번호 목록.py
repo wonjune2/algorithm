5b41eea872bed50180e963f6453975ea21039ac0
이원준 <zzzdnjswns@naver.com>
2021-10-27 23:40:50 +0900

    위장

--- programmers/전화번호 목록.py
+++ programmers/전화번호 목록.py
@@ -1,12 +1,10 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    print(phone_book)
     for i in range(len(phone_book)-1):
         if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
             answer = False
             break
-    print(answer)
     return answer
 
 phone_book = ["119", "97674223", "1195524421"]
