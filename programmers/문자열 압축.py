324acf627145b198f269e44bc3ec97338f6ba4e3
이원준 <zzzdnjswns@naver.com>
2021-10-29 07:17:30 +0900

    문자열 압축

--- programmers/문자열 압축.py
+++ programmers/문자열 압축.py
@@ -1,19 +1,33 @@
-def solution(s):
-    answer = 0
-    return answer
+s = "ababcdcdababcdcd"
 
-s = "abcabcabcdededededede"
-res = []
-dic = {}
-for j in range(i, len(s), i): 
-    if tmp==s[j:i+j]: 
-        cnt+=1 
-    else: 
-        if cnt!=1: 
-            b = b + str(cnt)+tmp 
-        else: 
-            b = b + tmp 
-            tmp=s[j:j+i] 
+def solution(s):
+    answer = []
+    if len(s) > 1:
+        for i in range(1, len(s) // 2 + 1,):
             cnt = 1
+            b = ''
+            temp = s[:i]
+            for j in range(i, len(s), i):
+                if temp == s[j:i+j]:
+                    cnt += 1
+                else:
+                    if cnt > 1:
+                        b += str(cnt) + temp
+                    else:
+                        b += temp
+                    temp = s[j:i+j]
+                    cnt = 1
+            if cnt > 1:
+                b += str(cnt) + temp
+            else:
+                b += temp
+            answer.append(len(b))
+        
+        return min(answer)
+    else:
+        return 1
+    
+print(solution(s))
+
 
 
