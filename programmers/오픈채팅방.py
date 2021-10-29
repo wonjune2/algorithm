4ad45989c64d38f5ad59663c26c558b645ba6a4d
이원준 <zzzdnjswns@naver.com>
2021-10-29 22:18:40 +0900

    오픈 채팅방

--- programmers/오픈채팅방.py
+++ programmers/오픈채팅방.py
@@ -15,7 +15,6 @@ def solution(record):
             answer.append(s)
         else:
             pass
-    print(answer)
     return answer
 
 record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
