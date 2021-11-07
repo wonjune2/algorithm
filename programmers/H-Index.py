c1e8373209a2f277f87b3a75fe8fb186b7c2bbbf
이원준 <zzzdnjswns@naver.com>
2021-11-07 22:50:29 +0900

    h-index

--- programmers/H-Index.py
+++ programmers/H-Index.py
@@ -7,21 +7,4 @@ def solution(citations):
     return 0
 
 citations = [3, 0, 6, 1, 5]
-# solution(citations)
-citations = sorted(citations)
-l = len(citations)
-print(l)
-for i in range(l):
-    print(citations[i])
-    if citations[i] >= l-i:
-        break
-# citations.sort()
-# answer = 0
-# for i in range(len(citations)):
-#     c = 0
-#     for j in range(i, len(citations)):
-#         if citations[i] <= citations[j]:
-#             c += 1
-#     if citations[i] <= c >= len(citations) - c:
-#         answer = citations[i]
-# print(answer)
+print(solution(citations))
