3776f38dc551cbd08300c572f61a966c9e7b6398
이원준 <zzzdnjswns@naver.com>
2021-11-09 00:49:00 +0900

    주식가격

---
+++ programmers/주식가격.py
@@ -0,0 +1,14 @@
+def solution(prices):
+    answer =[0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i]+=1
+            else:
+                answer[i]+=1
+                break
+    print(answer)
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+solution(prices)
