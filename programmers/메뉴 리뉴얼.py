5458a5af041998606038c988cdb440b745541edd
이원준 <zzzdnjswns@naver.com>
2021-11-14 21:34:37 +0900

    메뉴 리뉴얼

--- programmers/메뉴 리뉴얼.py
+++ programmers/메뉴 리뉴얼.py
@@ -1,18 +1,15 @@
 from itertools import combinations
 from collections import Counter
-
 def solution(orders, course):
     answer = []
-    for c in course:
+    for i in course:
         temp = []
         for order in orders:
-            combi = combinations(sorted(order), c)
-            temp += combi
+            temp += combinations(sorted(order), i)
         counter = Counter(temp)
         if len(counter) != 0 and max(counter.values()) != 1:
             answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
     return sorted(answer)
-
 orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
-course = [2,3,4]
-solution(orders, course)
+course = [2, 3, 4]
+print(solution(orders, course))
