d7cdfd5747977e6aa3c067952cca68883266e5f9
이원준 <zzzdnjswns@naver.com>
2021-11-19 20:57:43 +0900

    add_first

--- Doit/리스트/linked_list.py
+++ Doit/리스트/linked_list.py
@@ -23,3 +23,25 @@ class LinkedList:
     def __len__(self) -> int:
         """연결 리스트의 노드 개수를 반환"""
         return self.no
+
+    def search(self, data: Any) -> int:
+        """data와 값이 같은 노드를 검색"""
+        cnt = 0
+        ptr = self.head
+        while ptr is not None:
+            if ptr.data == data:
+                self.current = ptr
+                return cnt
+            cnt += 1
+            ptr = ptr.next
+        return -1
+
+    def __contains__(self, data: Any) -> bool:
+        """연결 리스트에 data가 포함되어 있는지 확인"""
+        return self.search(data) >= 0
+
+    def add_first(self, data: Any) -> None:
+        """맨 앞에 노드를 삽입"""
+        ptr = self.head # 삽입하기 전의 머리노드
+        self.head = self.current = Node(data, ptr)
+        self.no += 1
