235782860aa0d33088a82d9471331d468e3610ac
이원준 <zzzdnjswns@naver.com>
2021-11-20 20:07:10 +0900

    비밀지도

---
+++ programmers/비밀지도.py
@@ -0,0 +1,17 @@
+def solution(n, arr1, arr2):
+    answer = [''] * n
+    for i in range(n):
+        one = format(arr1[i], 'b').zfill(n)
+        two = format(arr2[i], 'b').zfill(n)
+        for j in range(len(one)):
+            if one[j] == '0' == two[j]:
+                answer[i] += ' '
+            else:
+                answer[i] += '#'
+    print(answer)
+    return answer
+
+n = 5
+arr1 = 	[9, 20, 28, 18, 11]
+arr2 = 	[30, 1, 21, 17, 28]
+solution(n, arr1, arr2)
