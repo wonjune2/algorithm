48efae1655c23dd16075ab993002f00eca4dc383
이원준 <zzzdnjswns@naver.com>
2021-11-23 18:25:09 +0900

    Doit LinkedListIterator

--- Doit/리스트/linked_list.py
+++ Doit/리스트/linked_list.py
@@ -112,3 +112,39 @@ class LinkedList:
             return False        # 이동할 수 없음
         self.current = self.current.next
         return True
+
+    def print_current_node(self) -> None:
+        """주목 노드를 출력"""
+        if self.current is None:
+            print('주목 노드가 존재하지 않습니다.')
+        else:
+            print(self.current.data)
+
+    def print(self) -> None:
+        """모든 노드를 출력"""
+        ptr = self.head
+
+        while ptr is not None:
+            print(ptr.data)
+            ptr = ptr.next
+
+    def __iter__(self) -> LinkedListIterator:
+        """이터레이터를 반환"""
+        return LinkedListIterator
+
+class LinkedListIterator:
+    """클래스 LinkedList의 이터레이터용 클래스"""
+
+    def __init__(self, head, Node):
+        self.current = head
+
+    def __iter__(self) -> LinkedListIterator:
+        return self
+
+    def __next__(self) -> Any:
+        if self.current is None:
+            raise StopIteration
+        else:
+            data = self.current.data
+            self.current = self.current.next
+            return data
