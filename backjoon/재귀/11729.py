787d4967c07bc58961697a6ea45bc09a6827773b
이원준 <zzzdnjswns@naver.com>
2021-12-11 22:25:30 +0900

    백준 11729

--- backjoon/재귀/11729.py
+++ backjoon/재귀/11729.py
@@ -1,14 +1,15 @@
-array = []
+result = []
+
 def hanoi(n, x, y):
     if n > 1:
         hanoi(n - 1, x, 6 - x - y)
-    array.append(str(x) + " " +str(y))
+    result.append(str(x) + " " + str(y))
     if n > 1:
         hanoi(n - 1, 6 - x - y, y)
-
+        
 n = int(input())
 
 hanoi(n, 1, 3)
-print(len(array))
-for i in array:
+print(len(result))
+for i in result:
     print(i)
--- backjoon/재귀/2447.py
+++ backjoon/재귀/2447.py
@@ -6,18 +6,16 @@ def get_stars(n):
         else:
             matrix.append(n[i % len(n)] * 3)
     return matrix
- 
- 
+
 star = ["***", "* *", "***"]
 n = int(input())
-e = 0
+k = 0
 
 while n != 3:
     n = int(n / 3)
-    e += 1
- 
-for i in range(e):
-    star = get_stars(star)
+    k += 1
 
+for i in range(k):
+    star = get_stars(star)
 for i in star:
     print(i)
