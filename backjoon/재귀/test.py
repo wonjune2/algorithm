24286571f5b2ddf253b4db84d86ec8f589d43452
이원준 <zzzdnjswns@naver.com>
2021-12-29 23:31:15 +0900

    프로그래머스 해시 복습

--- backjoon/재귀/test.py
+++ backjoon/재귀/test.py
@@ -1,18 +1,6 @@
-m, n = map(int, input().split())
-gra = [[0] * n for _ in range(m)]
-
-gra[1][0] = 1
-print(gra)
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# [0, 1, 0, 0, 0]
-# [0, 0, 0, 0, 0]
-# [0, 0, 0, 0, 0]
-# [0, 0, 0, 0, 0]
-
-[0, 0, 0], 
-[1, 0, 0], 
-[0, 0, 0], 
-[0, 0, 0]
+from collections import Counter
+v = [[1,4], [3,4], [3, 10]]
+v1 = [[1,1], [2,2], [1, 2]]
+x = Counter([v[0][0], v[1][0], v[2][0]]).most_common()
+y = Counter([v[0][1], v[1][1], v[2][1]]).most_common()
+print(x[1][0])
--- programmers/전화번호 목록.py
+++ programmers/전화번호 목록.py
@@ -1,11 +1,19 @@
+from collections import Counter
 def solution(phone_book):
-    answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
-            answer = False
-            break
-    return answer
+    hash_map = {}
+    for i in phone_book:
+        hash_map[i] = 1
+        
+    for i in phone_book:
+        number = ""
+        for j in i:
+            number += j
+            if number in hash_map and number != i:
+                return False
+
+    return True
+
+
 
 phone_book = ["119", "97674223", "1195524421"]
-solution(phone_book)
+
