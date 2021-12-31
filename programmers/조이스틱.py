aab106aa9a7d1a9e9853ebcf6b508e1ae2474533
이원준 <zzzdnjswns@naver.com>
2021-12-31 18:15:34 +0900

    programers 조이스틱

--- programmers/조이스틱.py
+++ programmers/조이스틱.py
@@ -1,17 +1,31 @@
 def solution(name):
+    change = [min(ord(i) - ord('A'), ord('Z') - ord(i) + 1) for i in name]
+    idx = 0
     answer = 0
-    a = 65
-    count = 0
-    for i in range(len(name)):
-        # if i < len(name) and 
-        s = ord(name[i]) - ord('A')
-        z = abs(ord(name[i]) - ord('Z')) + 1
-        if s < z:
-            count += s
+    
+    while True:
+        answer += change[idx]
+        change[idx] = 0
+        
+        if sum(change) == 0: break
+        
+        left = 1
+        right = 1
+        
+        while change[idx - left] == 0:
+            left += 1
+            
+        while change[idx + right] == 0:
+            right += 1
+            
+        if left >= right:
+            idx += right
+            answer += right
         else:
-            count += z
-        print(count)
+            idx -= left
+            answer += left
+            
     return answer
-
-name = "JEROEN"
+        
+name = "BBBAABB"
 solution(name)
