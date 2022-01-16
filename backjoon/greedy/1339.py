f5875c5568288a0f782501af0151addb5f15b500
wonejun <zzzdnjswns@naver.com>
2022-01-16 20:05:02 +0900

    백준 1339

---
+++ backjoon/greedy/1339.py
@@ -0,0 +1,25 @@
+from audioop import reverse
+from re import M
+import sys
+from collections import deque
+n = int(input())
+arr = [sys.stdin.readline().strip() for i in range(n)]
+# arr.sort(key=len, reverse=True)
+
+dic = {}
+
+for i in arr:
+    pow_val = len(i) - 1
+    for j in i:
+        if j in dic:
+            dic[j] += pow(10, pow_val)
+        else:
+            dic[j] = pow(10, pow_val)
+        pow_val -= 1
+dic = sorted(dic.values(), reverse=True)
+m = 9
+result = 0
+for i in dic:
+    result += i * m
+    m -= 1
+print(result)
